0f93d518646af548608f6562d18efe36a1b53877
fabiano <fabiano@fabiano-IS-1422>
2021-02-18 23:50:44 -0300

    Exercício de revisão 0.1

---
+++ Aprendendo_Linux/src/Exercicios_linux/Exercicio_revisao_01.java
@@ -0,0 +1,64 @@
+package Exercicios_linux;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 13/02/2021 
+*/
+public class Exercicio_revisao_01 {
+	/*
+	 * 	1) Abra o terminal.
+	 *     R:CONTROL+ALT+T
+	 *  
+	 *  2) Crie uma pasta de nome Ubuntu dentro da pasta Documentos.
+	 *     R: Passo 1: ls
+	 *        Passo 2: cd Documentos
+	 *        Passo 3: mkdir Ubuntu
+	 *        Passo 4: ls
+	 *       
+	 *   3) Mova esta pasta para o diretório pessoal. 
+	 *     R: Passo 1: mv Ubuntu ~
+	 *        Passo 2:cd ..
+	 *        Passo 3:ls
+	 *        
+	 *   4) Crie um arquivo vazio de nome teste.txt dentro da pasta Ubuntu
+	 *     R: Passo 1:cd Ubuntu
+	 *        Passo 2:touch teste.txt
+			  Passo 3:ls
+			 
+			  
+		  5) Renomeie este arquivo para Linux.txt 
+		    R: Passo 4:mv teste.txt Linux.txt
+			   Passo 5:ls
+			   
+		  6) Crie uma copia deste arquivo na pasta Downloads
+		    R: Passo 1:cp Linux.txt ~/Downloads
+		       Passo 2:ls
+		       Passo 3:cd ..
+		       passo 4: cd Downloads
+		       passo 5:ls
+		      
+		   7) Exiba todos comandos digitados no terminal
+		     R: Passo 1: cd ..
+		        Passo 2: history
+		    
+		   8) Execute a ajuda do comando ls
+		     R: Passo 1: ls --help
+		     
+		   9) Execute o manual do comando mv
+		     R: Passo 1: clear
+		        Passo 2: man mv
+		        
+		   10) Pare a execução do manual
+		     R: CONTROL + C
+		     
+		   11) Saia do Terminal utilizando sequência de teclas
+		     R: CONTROL + D
+		     
+		   12) Exclua a pasta Ubuntu
+		     R: Passo 1:ls
+		        Passo 2: rm -rf Ubuntu
+		        Passo 3:ls
+		        Passo 4:clear
+		        Passo 5:exit
+	 */
+}
