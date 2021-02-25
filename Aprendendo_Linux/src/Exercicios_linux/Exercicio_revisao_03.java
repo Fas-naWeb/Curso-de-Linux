bf794b7508f04ff7c8f35869abbad028549d67c4
fabiano <fabiano@fabiano-IS-1422>
2021-02-25 00:28:51 -0300

    Exercicio de revisão 4

---
+++ Aprendendo_Linux/src/Exercicios_linux/Exercicio_revisao_03.java
@@ -0,0 +1,60 @@
+package Exercicios_linux;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 19/02/2021 
+*/
+public class Exercicio_revisao_03 {
+/*
+   1) Exiba todos os hardware que existem na máquina com paginação
+   R:passo 1:CRTL + ALT + T -> Para abrir o terminal
+     passo 2:lshw | more -> exibe todos os hardware do computador
+     
+   2) Salve a saida do comando anterior no arquivo minhamáquina.txt
+   R:lshw > minhamáquina.txt -> joga a saida do comando para um arquivo de texto
+   
+   3) Exiba informações sobre a memória física e virtual na sua máquina.
+   R:free -> Exibe sobre a memória fisica e virtual da máquina
+   
+   4) Adiciona a saida do comando anterior no arquivo minhamáquina.txt
+   R:free >> minhamáquina.txt
+   
+   5) Exiba todas placas pci conectadas
+   R: lspci -> Exibe todas placas pci conectadas na maquina. 
+   
+   6)Adiciona a saida do comando anterior no arquivo minhamáquina.txt
+   R: lspci >> minhamáquina.txt
+   
+   7) Exiba todos dispositivos usb conectados
+   R: lsusb -> Exibe todos dispositivos usb conectados
+   
+   8) Adiciona a saida do comando anterior no arquivo minhamáquina.txt
+   R: lsusb >> minhamáquina.txt
+   
+   9) Exiba as informações sobre processador
+   R: lscpuinfo
+   
+   10) Adiciona a saida do comando anterior no arquivo minhamáquina.txt
+   R: lscpuinfo >> minhamáquinma.txt
+   
+   11) Exiba o nome do kernel
+   R: uname
+   
+   12) Adiciona a saida do comando anterior no arquivo minhamáquina.txt
+   R:uname >> minhamáquina.txt
+   
+   13) Exiba a versão do kernel
+   R:uname -r
+   
+   14) Adiciona a saida do comando anterior no arquivo minhamáquina.txt
+   R: uname -r >> minhamáquina.txt
+   
+   15) Exiba a arquitetura do kernel
+   R: arch
+   
+   16) Adiciona a saida do comando anterior no arquivo minhamáquina.txt
+   R: arch >> minhamáquina.txt
+   
+   
+ */
+}
