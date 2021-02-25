8d710749ad6a74cfe3ee6a2c21427bb71ca5a0c4
fabiano <fabiano@fabiano-IS-1422>
2021-02-25 01:03:07 -0300

    Exercicio de revisão 4

---
+++ Aprendendo_Linux/src/Exercicios_linux/Exercicio_04.java
@@ -0,0 +1,48 @@
+package Exercicios_linux;
+/*
+	Autor: Fabiano Alves Santos
+	Email: fasnaweb2020@gmail.com
+	Data: 19/02/2021 
+*/
+public class Exercicio_04 {
+
+	public static void main(String[] args) {
+/*
+   Exercícios Práticos
+1) Crie um arquivo de aularedes.txt
+   R: Passo 1:CRTL + ALT + T -> Abre seu terminal
+      Passo 2:ls -> lista os diretórios pa pasta que vc se encontra no momento
+      Passo 3:touch aularedes.txt
+      
+2) Exiba o número de IP da rede no terminal
+   R:Passo 1: ifconfig
+     Passo 2: Você vai procurar por INET no meu caso o endereço é 192.168.1.8 ou
+     Passo 3: Esar o comando hostname -I
+     
+3) Adicione a saída do comando anterior ao arquivo aula redes.txt
+   R:ifconfig > aularede.txt
+   
+4) Exiba o número de IP Loopback no terminal
+   R:hostname -i Desse computador que estou utilizando é 127.0.1.1
+   
+5) Adicione a saída do comando anterior ao arquivo aula redes.txt
+   R:hostname -i >> aularedes.txt
+   
+6) Exiba Informações DNS sobre o host www.pudim.com.br
+   R:
+7) Adicione a saída do comando anterior ao arquivo aula redes.txt
+8) Exiba Informações do Usuário logado na rede
+9) Adicione a saída do comando anterior ao arquivo aula redes.txt
+10) Execute um teste no host www.pudim.com.br com 6 pacotes
+11) Adicione a saída do comando anterior ao arquivo aula redes.txt
+12) Trace a Rota do seu computador até o host www.pudim.com.br
+13) Adicione a saída do comando anterior ao arquivo aula redes.txt
+14) Exiba Informações sobre Interfaces de Rede e Endereços IP no terminal
+15) Adicione a saída do comando anterior ao arquivo aula redes.txt
+16) Limpe o terminal
+17) Imprima o arquivo aularedes.txt com paginação no terminal
+ */
+
+	}
+
+}
