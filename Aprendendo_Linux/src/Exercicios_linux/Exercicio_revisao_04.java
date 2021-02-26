3e8d4c784c2fef873e3b18777e48bee3e5df7d0f
fabiano <fabiano@fabiano-IS-1422>
2021-02-25 22:26:21 -0300

    Exercicio de revisão 04

--- Aprendendo_Linux/src/Exercicios_linux/Exercicio_revisao_04.java
+++ Aprendendo_Linux/src/Exercicios_linux/Exercicio_revisao_04.java
@@ -29,18 +29,40 @@ public class Exercicio_revisao_04 {
    R:hostname -i >> aularedes.txt
    
 6) Exiba Informações DNS sobre o host www.pudim.com.br
-   R:
+   R:dig www.pudim.com.br
+   
 7) Adicione a saída do comando anterior ao arquivo aula redes.txt
+   R:dig www.pudim.com.br >> aularedes.txt
+   
 8) Exiba Informações do Usuário logado na rede
+   R:who
+   
 9) Adicione a saída do comando anterior ao arquivo aula redes.txt
-10) Execute um teste no host www.pudim.com.br com 6 pacotes
+   R:who >> aularedes.txt
+   
+10 Execute um teste no host www.pudim.com.br com 6 pacotes
+   R:ping www.pudim.com.br -w 6
+   
 11) Adicione a saída do comando anterior ao arquivo aula redes.txt
+   R:ping www.pudim.com.br -w 6 >> aularedes.txt
+
 12) Trace a Rota do seu computador até o host www.pudim.com.br
+   R:traceroute www.pudim.com.br
+   
 13) Adicione a saída do comando anterior ao arquivo aula redes.txt
+   R:traceroute www.pudim.com.br >> aularede.txt
+   
 14) Exiba Informações sobre Interfaces de Rede e Endereços IP no terminal
+   R:
+   
 15) Adicione a saída do comando anterior ao arquivo aula redes.txt
+   R:
+   
 16) Limpe o terminal
+   R:clear
+   
 17) Imprima o arquivo aularedes.txt com paginação no terminal
+   R:less aularedes.txt more
  */
 
 	}
