0b4423f3d79a497bbce9c53f7474b6ab77c3ccb9
fabiano <fabiano@fabiano-IS-1422>
2021-03-10 22:17:37 -0300

    Exercicio de revisão 07

---
+++ Aprendendo_Linux/src/Exercicios_linux/Exercicio_revisao_07.java
@@ -0,0 +1,27 @@
+package Exercicios_linux;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 10/03/2021 
+*/
+public class Exercicio_revisao_07 {
+
+	public static void main(String[] args) {
+	/*
+	  1) Instale o wiresshark com o gerênciador apt
+	  2) Atualize o wiresshark
+	  3) Remova o wiresshark
+	  4) Atualize o sistema
+	  5) Baixe e instale o pacote deb do postresql
+	  6) Remova o postresql
+	  COM FEDORA
+	  7) va para o fedora
+	  8) Baixe o pacote postresqlcom o gerenciador de pacotes rpm
+	  9) Remova o pacote postresql com o gerenciador rpm
+	 10) Com o instalador yum intale o pacote postresql
+	 11) Remova o pacote postresql com o instalador yum
+	  
+	 */
+	}
+
+}
