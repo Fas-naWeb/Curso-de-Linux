e9edebff4a3bc3313289af7edd2c4d90ee2a2616
fabiano <fabiano@fabiano-IS-1422>
2021-03-11 18:32:54 -0300

    Shell Script 08

---
+++ Aprendendo_Linux/src/Exercicios_linux/ShellScript.java
@@ -0,0 +1,75 @@
+package Exercicios_linux;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 13/02/2021 
+*/
+public class ShellScript {
+/*
+   OQUE É SHELL SCRIPT ?
+   Shell em computação é um interface de usuário para acessar
+   os serviços de um sistema operacional
+   
+   Script é uma linguagem de programação que executa no sistema em
+   tempo de execução, muito utilizada para automação.
+   
+   MUDAR DE DIRETÓRIO
+   Comando cd -> Serve para mudar de diretório
+   
+   cd / Abre o diretório raiz do sistema
+   
+   cd ~ Abre o diretório do usuário correte.O usuário logado naquele instante
+   
+   cd .. Volta um diretório
+   
+   LISTAR ARQUIVOS
+   Comando serve para listar diretórios
+   
+   ls -l Lista arquivos em coluna detalhado
+   
+   ls -a Lista conteudo até os arquivos ocultos
+   
+   ls -s Lista o conteudo com tamanho alocado de cada arquivo em bloco.
+   
+   COMANDO TOUCH
+   É utilizado para criar arquivos vazios ou alterar data e hora dos arquivos
+   
+   Criar arquivos multiplos 
+   touch nomeFile1.txt nomeFile2.txt nomeFile3.txt
+   
+   Alterar hora de acesso
+   touch -a nomeFile1.txt
+   
+   Alterar hora da modificação
+   touch -m nomeFile1.txt
+   
+   Alterar hora de acesso sem criar um novo arquivo	
+   touch -c nomeFile1.txt
+   
+   COMANDO CAT 
+   Comando para visualizar o conteudo do arquivo
+   
+   cat nomeFile1.txt -> visualiza o conteudo do arquivo
+   
+   cat nomeFile1.txt > nomeFile2.txt -> Direciona o conteudo do primeiro arquivo para o segundo arquivo
+   
+   cat >> nomeFile1.txt
+   
+   MOVER ARQUIVOS
+   O comando mv serve tanto para mover arquivos quanto para renomear arquivos
+   
+   mv nomeFile1.txt nomeFile2.txt -> renomeia o primeiro arquivo com o nome do segundo.
+   
+   SITUAÇÕES 
+   -i Confirma a alteração
+   -n Sem subistituição do arquivo
+   -b Subistitua o arquivo
+   
+   
+   
+   
+   
+   
+   
+ */
+}
