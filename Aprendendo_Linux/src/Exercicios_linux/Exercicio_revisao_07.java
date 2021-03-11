09b48729727773f59be4cc28461545a3b80311d5
fabiano <fabiano@fabiano-IS-1422>
2021-03-10 23:13:48 -0300

    Exercicio de revisão 07

--- Aprendendo_Linux/src/Exercicios_linux/Exercicio_revisao_07.java
+++ Aprendendo_Linux/src/Exercicios_linux/Exercicio_revisao_07.java
@@ -8,20 +8,41 @@ public class Exercicio_revisao_07 {
 
 	public static void main(String[] args) {
 	/*
-	  1) Instale o wiresshark com o gerênciador apt
-	  2) Atualize o wiresshark
-	  3) Remova o wiresshark
+	  1) Instale o wireshark com o gerênciador apt
+	     R:sudo apt install wireshark
+	     
+	  2) Atualize o wireshark
+	     R:sudo apt upgrade wireshark
+	     
+	  3) Remova o wireshark
+	     R:sudo remove wireshark
+	     
 	  4) Atualize o sistema
+	     R:sudo apt upgrade wireshark
+	     
 	  5) Baixe e instale o pacote deb do postresql
+	     R:dpkg -i postresql.db
+	     
 	  6) Remova o postresql
+	     R: dpkg -r postresql.fdw
+	     
 	  COM FEDORA
+	  
 	  7) va para o fedora
+	     R: sudo rpm -ivh --nodps postresql.rpm
+	     
 	  8) Baixe o pacote postresqlcom o gerenciador de pacotes rpm
+	     R:sudo yum install postresql
+	     
 	  9) Remova o pacote postresql com o gerenciador rpm
+	     R:sudo yum remove postresql
 	 10) Com o instalador yum intale o pacote postresql
+	     R:sudo yum install postresql
 	 11) Remova o pacote postresql com o instalador yum
+	     R:sudo yum remove postresql
 	  
 	 */
 	}
 
 }
+
