da9843d57ad3c989ec2cda033735af3caf3e10fd
fabiano <fabiano@fabiano-IS-1422>
2021-03-11 23:04:34 -0300

    Estrutura de dados

---
+++ Aprendendo_Linux/src/Exercicios_linux/EstruturaDeDados.java
@@ -0,0 +1,16 @@
+package Exercicios_linux;
+/*
+	Autor:Fabiano Alves Santos
+	Email:fasnaweb2020@gmail.com
+	Data: 11/03/2021 
+*/
+public class EstruturaDeDados {
+/*
+   O QUE É ESTRUTURA DE DADOS
+   Estrutura de dados é uma estrutura organizada de dados na memória do 
+   computador ou em qualquer dispositivo de armazenamento, de forma que
+   os dados possam ser utilizados de forma correta.  
+ 
+ */
+	
+}
