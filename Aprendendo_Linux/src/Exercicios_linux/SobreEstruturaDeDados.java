4d4e683e529946e02292b40c730983e6b9c4a45f
fabiano <fabiano@fabiano-IS-1422>
2021-03-12 23:39:18 -0300

    Sobre Estrutura de Dados 10

--- Aprendendo_Linux/src/Exercicios_linux/SobreEstruturaDeDados.java
+++ Aprendendo_Linux/src/Exercicios_linux/SobreEstruturaDeDados.java
@@ -11,6 +11,81 @@ public class SobreEstruturaDeDados {
    computador ou em qualquer dispositivo de armazenamento, de forma que
    os dados possam ser utilizados de forma correta.  
  
+   Essas estruturas encontram muitas aplicações no desenvolvimento de sistemas 
+   sendo que algumas são altamenter especializadas em tarefas especificas.Usando
+   as estruturas adequadas através de algoritmos podemos trabalhar com uma grande
+   quantidades de dados como aplicações em banco de dados ou serviços de busca.  
+   
+   ALGORITMO
+   Um algoritmo é um conjunto de instruções estruturadas e ordenadas,seu objetivo é
+   realizar uma tarefa ou operação especifica.
+   
+   Os algoritmos são utilizados para manipular dados nas estruturas de varias formas
+   como por exemplo inserir,excluir,procurar e ordenar dados.
+   
+   EM UMA ESTRUTURA DE DADOS DEVEMOS:
+   Saber como realizar um conjunto de operações básicas,como por exemplos:
+   
+   inserir dados 
+   excluir dados 
+   percorrer dados os itens constituintes da estrutura para visualização
+   classificar,que se resume em colocar os itens de dados em uma determinada ordem 
+   numerica ou alfabetica e ec...
+   
+   PRINCIPAIS ESTRUTURA DE DADOS
+   Vetores e matrizes
+   Registro
+   Lista
+   Pilha
+   Fila
+   Árvore
+   Tabela hash
+   Grafos
+   
+   MATRIZES E VETORES
+   São estrutura de dados simples que podem auxiliar quando há muitas variáveis do mesmo
+   tipo em um algoritmo.
+   
+   VETOR
+   Vetor ou array uni-dimencional é uma variável que armazena várias variáveis do mesmo tipo
+   O vetor é uma extrutura de dados indexada,que pode armazenar uma determinada quantidade de
+   valores do mesmo tipo  
+   
+   MATRIZ
+   Matriz ou array multidimencional é um vetor de vetores.
+   Uma matriz é um vator que possui duas ou mais dimenções.
+   
+   REGISTRO
+   Enquanto array nos permite armazenar vários dados de um unico tipo de dados,oregistro nos
+   permite armazenar mais de um tipo de dado. 
+   
+   LISTAS
+   A diferença entre listas e arrays é a de que as listas possuem tamanho ajustável,enquanto
+   arrays possuem tamanho fixo.
+   
+   EXISTE DOIS TIPOS DE LISTAS 
+   Listas ligadas -> Existe os nós onde cada um dos nós conhece o valor que esta sendo armazenado
+   em seu interior além de conhecer o elemento posterior a ele:Por isso ela é chamada de "lista ligada"
+   pois os nós são amarrados com essa indicação de qual é o proximo nó.
+   
+   
+   Listas duplamente ligadas -> A grande diferença das listas duplamnete ligadas para as lista ligada
+   é que elas são bidirecionais.Vimos que naturalmente,não conseguimos "andar para trás"em listas ligadas
+   pois os nós de uma lista ligadas sabem somente quem é o proximo elemento.Nas listas duplamnente ligadas
+   os nós sabem quem é o elemento anterior o que permite a navegação reversa.
+   
+   PILHAS
+   Uma pilha é uma estrutura de dados que serve como uma coleção de elementos e permite o acesso a somente 
+   um item de dados armazenados.
+   
+   O acesso aos itens de uma pilha é restrito-somente um item pode ser lido ou removido por vez
+   
+   TEMOS DOIS TIPOS DE PILHAS 
+   Lifo -> Significa que o ultimo que entra é o primeiro que sai,apresenta o seguinte critério
+   :O primeiro elemento a ser retirado e o ultimo elemento que foi inserido.
+    
+   Fifo -> Primeiro que entra é o primeiro que sai,apreseta o seguinte critério:O primeiro elemento
+   a ser retirado é o primeiro que foi inserido.
  */
 	
 }
